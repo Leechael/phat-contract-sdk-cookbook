19c6ad38a8fa4bc99374d4f17c375bdd048c576b
Leechael Yim <yanleech@gmail.com>
2023-10-19 21:49:39 +0800

    imp: update the upload example with auto-deposit

--- upload.js
+++ upload.js
@@ -1,165 +1,33 @@
 require('dotenv').config()
 
-const util = require('util')
 const fs = require('fs')
-const Phala = require('@phala/sdk')
-const { typeDefinitions } = require('@polkadot/types');
+const { signAndSend, signCertificate, OnChainRegistry, options, PinkCodePromise } = require('@phala/sdk')
 const { ApiPromise, Keyring, WsProvider } = require('@polkadot/api')
 const BN = require('bn.js')
 
-
-function inspect(obj) {
-  return util.inspect(obj, false, null, true)
-}
-
-function hex(b) {
-    if (typeof b != "string") {
-        b = Buffer.from(b).toString('hex');
-    }
-    if (!b.startsWith('0x')) {
-        return '0x' + b;
-    } else {
-        return b;
-    }
-}
-
-async function sleep(t) {
-  await new Promise(resolve => {
-      setTimeout(resolve, t);
-  });
-}
-
-const signAndSend = (target, signer) => {
-  return new Promise(async (resolve, reject) => {
-    // Ready -> Broadcast -> InBlock -> Finalized
-    const unsub = await target.signAndSend(
-      signer, (result) => {
-        if (result.status.isInBlock) {
-          let error;
-          for (const e of result.events) {
-            const { event: { data, method, section } } = e;
-            if (section === 'system' && method === 'ExtrinsicFailed') {
-              error = data[0];
-            }
-          }
-          // @ts-ignore
-          unsub();
-          if (error) {
-            reject(error);
-          } else {
-            resolve({
-              hash: result.status.asInBlock.toString(),
-              // @ts-ignore
-              events: result.toHuman().events,
-              result,
-            });
-          }
-        } else if (result.status.isInvalid) {
-          // @ts-ignore
-          unsub();
-          reject('Invalid transaction');
-        }
-      }
-    )
-  })
-}
-
-async function txAccepted(txBuilder, signer, shouldSucceed = true) {
-    return await new Promise(async (resolve, _reject) => {
-        const unsub = await txBuilder.signAndSend(signer, { nonce: -1 }, (result) => {
-            if (result.status.isInBlock) {
-                let error;
-                for (const e of result.events) {
-                    const { event: { data, method, section } } = e;
-                    if (section === 'system' && method === 'ExtrinsicFailed') {
-                        if (shouldSucceed) {
-                            error = data[0];
-                        } else {
-                            unsub();
-                            resolve(error);
-                        }
-                    }
-                }
-                if (error) {
-                    console.error(`Extrinsic failed with error: ${error}`);
-                }
-                unsub();
-                resolve({
-                    hash: result.status.asInBlock,
-                    events: result.events,
-                });
-            } else if (result.status.isInvalid) {
-                assert.fail('Invalid transaction');
-                unsub();
-                resolve();
-            }
-        });
-    });
-}
-
-async function checkUntil(async_fn, timeout) {
-    const t0 = new Date().getTime();
-    while (true) {
-        if (await async_fn()) {
-            return;
-        }
-        const t = new Date().getTime();
-        if (t - t0 >= timeout) {
-            throw new Error('timeout');
-        }
-        await sleep(100);
-    }
-}
-
-async function checkUntilEq(async_fn, expected, timeout, verbose=true) {
-  const t0 = new Date().getTime();
-  let lastActual = undefined;
-  while (true) {
-      const actual = await async_fn();
-      if (actual === expected) {
-          return;
-      }
-      if (actual !== lastActual && verbose) {
-          console.debug(`Waiting... (current = ${actual}, expected = ${expected})`)
-          lastActual = actual;
-      }
-      const t = new Date().getTime();
-      if (t - t0 >= timeout) {
-          throw new Error('timeout');
-      }
-      await sleep(100);
-  }
-}
-
-//
-//
-//
-
-async function upload_and_instantiate_contract() {
+async function main() {
   const endpoint = process.env.ENDPOINT
   const account = process.env.POLKADOT_ACCOUNT
   if (!endpoint || !account) {
     console.log('Please create your own .env file with `ENDPOINT` and `POLKADOT_ACCOUNT`.')
     return process.exit(1)
   }
-  const targetFile = process.argv[2]
-  if (!endpoint || !targetFile) {
-    console.log('Usage: node upload.js [path/to/yours.contract]')
-    return process.exit(1)
-  }
+  const targetFile = 'abis/fat_badges.contract'
+  // const targetFile = process.argv[2]
+  // if (!endpoint || !targetFile) {
+  //   console.log('Usage: node upload.js [path/to/yours.contract]')
+  //   return process.exit(1)
+  // }
   if (!fs.existsSync(targetFile)) {
     console.log(`${targetFile} not exists.`)
     return process.exit(1)
   }
   const contractFile = JSON.parse(fs.readFileSync(targetFile))
 
-  // const abi = new Abi(contractFile)
-  // console.log(abi.constructors)
-
   // Initialization
   console.log('Connecting to', endpoint, '...')
   const provider = new WsProvider(endpoint)
-  const api = await ApiPromise.create(Phala.options({ provider, noInitWarn: true }))
+  const api = await ApiPromise.create(options({ provider, noInitWarn: true }))
   console.log('Connected.')
 
   const keyring = new Keyring({ type: 'sr25519' })
@@ -172,7 +40,7 @@ async function upload_and_instantiate_contract() {
   }
   console.log(`Account ${user.address} has ${free} PHA.`)
 
-  const phatRegistry = await Phala.OnChainRegistry.create(api)
+  const phatRegistry = await OnChainRegistry.create(api)
 
   const clusterId = phatRegistry.clusterId
   // const clusterInfo = phatRegistry.clusterInfo
@@ -180,7 +48,7 @@ async function upload_and_instantiate_contract() {
   console.log('Cluster ID:', clusterId)
   console.log('Pruntime Endpoint URL:', pruntimeURL)
 
-  const balance = await phatRegistry.getClusterBalance(user, user.address)
+  const balance = await phatRegistry.getClusterBalance(user.address)
   console.log('Cluster Balance:', balance.total.toPrimitive() / 1e12, balance.free.toPrimitive() / 1e12)
 
   if ((balance.free.toPrimitive() / 1e12) < 10) {
@@ -194,39 +62,26 @@ async function upload_and_instantiate_contract() {
     }
   }
 
+  const cert = await signCertificate({ pair })
+  ///
+  /// All prepare conditions ready.
+  ///
+
   //
   // Step 1: Upload with PinkCodePromise
   //
   console.log('Upload codes...')
-  const codePromise = new Phala.PinkCodePromise(api, phatRegistry, contractFile, contractFile.source.wasm)
-  const { result: uploadResult } = await signAndSend(codePromise.tx.new({}), user)
-  {
-    const cert = await Phala.signCertificate({ pair, api })
-    await uploadResult.waitFinalized(user, cert, 120_000)
-  }
-
+  const codePromise = new PinkCodePromise(api, phatRegistry, contractFile, contractFile.source.wasm)
+  const uploadResult = await signAndSend(codePromise.tx.new(), user)
+  await uploadResult.waitFinalized(user, cert)
   console.log('Code ready in cluster.')
 
   //
   // Step 2: instantiate with PinkBlueprintPromise
   //
   console.log('Instantiating...')
-  let instantiateResult
-  try {
-    const { blueprint } = uploadResult // Or use `blueprintPromise` instead: new Phala.PinkBlueprintPromise(api, phatRegistry, contractFile, contractFile.source.hash)
-    const cert = await Phala.signCertificate({ pair, api })
-    const { gasRequired, storageDeposit, salt } = await blueprint.query.new(user.address, { cert }) // Support instantiate arguments.
-    const response = await signAndSend(
-      blueprint.tx.new({ gasLimit: gasRequired.refTime, storageDepositLimit: storageDeposit.isCharge ? storageDeposit.asCharge : null, salt }),
-      user
-    )
-    instantiateResult = response.result
-    await instantiateResult.waitFinalized()
-  } catch (err) {
-    console.log(`Instantiate failed: ${err}`)
-    console.error(err)
-    return process.exit(1)
-  }
+  const instantiateResult = await uploadResult.blueprint.send.new({ pair, cert, address: cert.address })
+  await instantiateResult.waitFinalized()
 
   const { contractId, contract } = instantiateResult
   console.log('Contract uploaded & instantiated: ', contractId)
@@ -240,39 +95,24 @@ async function upload_and_instantiate_contract() {
   //
   // query test. 
   //
-  const cert = await Phala.signCertificate({ pair, api })
   const totalQueryResponse = await contract.query.getTotalBadges(user.address, { cert })
   const total = totalQueryResponse.output.toJSON()
   console.log('total:', total)
 
-  {
-    let name = `Badge${new Date().getTime()}`
-    // // costs estimation
-    const cert = await Phala.signCertificate({ pair, api })
-    const { gasRequired, storageDeposit } = await contract.query.newBadge(pair.address, { cert }, name)
-    console.log('gasRequired & storageDeposit: ', gasRequired.refTime.toBn().div(new BN(1e10)).toNumber() / 100, storageDeposit.asCharge.toHuman())
+  //
+  // trx with auto-deposit test.
+  //
+  const name = `Badge${new Date().getTime()}`
+  const result = await contract.send.newBadge({ pair, cert, address: cert.address }, name)
+  await result.waitFinalized()
+  console.log('trx submited')
 
-    // transaction / extrinct
-    const options = {
-      gasLimit: gasRequired.refTime,
-      storageDepositLimit: storageDeposit.isCharge ? storageDeposit.asCharge : null,
-    }
-    const { result } = await signAndSend(contract.tx.newBadge(options, name), pair)
-    await result.waitFinalized()
-    console.log('submited')
-  }
-
-  {
-    // query
-    const cert = await Phala.signCertificate({ pair, api })
-    const totalQueryResponse = await contract.query.getTotalBadges(pair.address, { cert })
-    const total = totalQueryResponse.output.toJSON()
-    console.log('total:', total)
-  }
+  //
+  // query data after trx.
+  //
+  const { output: after } = await contract.query.getTotalBadges(pair.address, { cert })
+  console.log('total:', after.toJSON())
 }
-}
-
-const main = upload_and_instantiate_contract
 
 main().then(() => process.exit(0)).catch(err => {
   console.error(err)
